6663c9bf5f3a63e29c7f90bdb359d7d62da4e1c8
Peter McClelland <peterwmcclelland@gmail.com>
2022-09-26 14:45:35 -0500

    update

---
+++ src/app.js
@@ -0,0 +1,76 @@
+/* global instantsearch */
+
+import { hitTemplate } from "./helpers";
+
+const search = instantsearch({
+  appId: "B1G2GM9NG0",
+  apiKey: "aadef574be1f9252bb48d4ea09b5cfe5",
+  indexName: "Index_PinIt",
+  searchParameters: {
+    hitsPerPage: 3,
+    attributesToSnippet: ["content:14"],
+    snippetEllipsisText: " [...]"
+  }
+});
+
+// Uncomment the following widget to add hits list.
+
+ search.addWidget(
+  instantsearch.widgets.hits({
+    container: "#hits",
+    templates: {
+      empty: "No results found.",
+      item(hit) {
+        return hitTemplate(hit);
+      }
+    }
+  })
+); 
+
+// Uncomment the following widget to add a search bar.
+
+ search.addWidget(
+  instantsearch.widgets.searchBox({
+    container: "#searchbox",
+    placeholder: "Search articles",
+    autofocus: false
+  })
+); 
+
+// Uncomment the following widget to add search stats.
+
+search.addWidget(
+  instantsearch.widgets.stats({
+    container: "#stats",
+    templates: {
+      body(hit) {
+        return `<span role="img" aria-label="emoji">⚡️</span> <strong>${hit.nbHits}</strong> results found ${
+          hit.query != "" ? `for <strong>"${hit.query}"</strong>` : ``
+        } in <strong>${hit.processingTimeMS}ms</strong>`;
+      }
+    }
+  })
+); 
+
+// Uncomment the following widget to add categories list.
+
+ search.addWidget(
+  instantsearch.widgets.refinementList({
+    container: "#categories",
+    attributeName: "categories",
+    autoHideContainer: false,
+    templates: {
+      header: "Categories"
+    }
+  })
+); 
+
+// Uncomment the following widget to add pagination.
+
+ search.addWidget(
+  instantsearch.widgets.pagination({
+    container: "#pagination"
+  })
+); 
+
+search.start();
